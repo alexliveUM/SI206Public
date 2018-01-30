175f4b12045c76a174e08c3466e09f6ae143f3a4
Geoff Pottow <gpottow@umich.edu>
2018-01-30 09:30:38 -0500

    added info about me

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ print('My name is Alex Li.')
 print('I was born in November')
 print('My name is Geoff Pottow')
 print('I was born in July')
+
+
+print('My favorite food is paella')
+print('My favorite type of music is Rock')
+print('My astrological sign is Cancer')
